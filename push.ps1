955f820c0bd68cd5fe6094587df18fedc81454b6
Milli <onethenumbernottheword@gmail.com>
2023-12-07 11:52:07 +0100

    rename and update push.ps1

--- push.ps1
+++ push.ps1
@@ -4,16 +4,23 @@ git add .
 
 $fileNames = Get-ChildItem -File | Select-Object -ExpandProperty Name
 
-$highestNumber = 0
+$day = 0
 foreach ($fileName in $fileNames) {
     if ($fileName -match "^\d+") {
         $number = [int]$matches[0]
-        if ($number -gt $highestNumber) {
-            $highestNumber = $number
+        if ($number -gt $day) {
+            $day = $number
         }
     }
 }
 
-git commit -m "day $highestNumber"
+git commit -m "day $day"
 git push --set-upstream gitlab main
 git push --set-upstream origin main
+
+# Create files for the next day
+$dd = "{0:D2}" -f ++$day
+
+Copy-Item -Path "00 - Template.py" -Destination "$dd.py"
+
+New-Item -Path "data" -Name "$dd.txt"
